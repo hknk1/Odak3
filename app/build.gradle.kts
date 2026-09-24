a9caef023de7f7a3f01f4280936f9372562d2d4e
hknk1 <105922358+hknk1@users.noreply.github.com>
2026-09-24 17:28:30 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.odak3.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.odak3.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
+}
